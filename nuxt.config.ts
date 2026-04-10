3c129fe7a117a7faca1163ba36d9b53fea6a671f
boluocuixue <2936178903@qq.com>
2026-04-10 15:37:35 +0800

    feat:初始化项目

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss',
+    '@sidebase/nuxt-auth',
+    '@element-plus/nuxt'
+  ]
+})
